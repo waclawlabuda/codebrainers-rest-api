eaae37dd0d28516ce45b612f0d0a52035f465733
waclaw.labuda <waclaw.labuda@gmail.com>
2022-04-05 07:34:32 +0200

    modify index.js

--- index.js
+++ index.js
@@ -2,26 +2,27 @@ require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
-const mongoString = process.env.DATABASE_URL;
+const mongoString = 'mongodb+srv://admin:adminqwerty@cluster0.ba0v0.mongodb.net/test';
 
 mongoose.connect(mongoString);
 const database = mongoose.connection;
 
 database.on('error', (error) => {
-    console.log(error)
-})
+    console.log(error);
+});
 
 database.once('connected', () => {
     console.log('Database Connected');
-})
+});
+
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 const routes = require('./routes/routes');
 
-app.use('/api', routes)
+app.use('/api', routes);
 
 app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
-})
+    console.log(`Server Started at ${3000}`);
+});
